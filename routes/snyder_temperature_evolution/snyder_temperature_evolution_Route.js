725e3a76e40aee7a00177aaabf46d09d1f15f070
raito <raitokomniak@gmail.com>
2022-11-19 14:14:53 +0200

    sort typo

--- routes/snyder_temperature_evolution/snyder_temperature_evolution_Route.js
+++ routes/snyder_temperature_evolution/snyder_temperature_evolution_Route.js
@@ -5,7 +5,7 @@ const snyder_temperature_evolution = require('../../models/snyder_temperature_ev
 // Get all data
 router.get('/', async (req, res) => {
     try {
-        const result = await snyder_temperature_evolution.find().sort({Year: 1})
+        const result = await snyder_temperature_evolution.find().sort({kyr_BP: 1})
         res.status(200).json(result)
     } catch (err) {res.status(500).json({message: err.message})}
 })
