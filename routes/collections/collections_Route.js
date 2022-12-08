1d2cc1b6569420738c5709b8be45dae0b096b741
raito <raitokomniak@gmail.com>
2022-12-08 10:45:27 +0200

    delete all collections by user when deleting user

--- routes/collections/collections_Route.js
+++ routes/collections/collections_Route.js
@@ -17,45 +17,45 @@ router.get('/', async (req, res) => {
 // Get all by user
 router.get('/:username', async (req, res) => {
     try {
-        const result = await Collections.find({createdBy: {$gte: req.params.username}})
+        const result = await Collections.find({createdBy: req.params.username})
         res.status(200).json(result)
     } catch (err) {res.status(500).json({message: err.message})}
 })
 
 
+
 router.post('/create', async (req, res) => {
+    let collID
+    try{
     User.findOne({
         username: req.body.username
       })
-        .exec((err, user) => {
-            u = user
+        .exec((error) => {
             let token = req.headers["x-access-token"];
-            if (!token) {
-            return res.status(403).send({ message: "No token provided!" });
-            }
-        
+            if (!token) return res.status(403).send({ message: "No token provided!" });
+
             jwt.verify(token, config.secret, (err, decoded) => {
-            if (err) {
-                return res.status(401).send({ message: "Unauthorized!" });
-            }
-            req.userId = decoded.id;
-            createColl(req, res, u)
+                if (err) return res.status(401).send({ message: "Unauthorized!" });
+                req.userId = decoded.id;
+                try {
+                    var newCollection = new Collections({ formatType: req.body.formatType, visualizations: req.body.visualizations, createdBy: req.body.username});
+                    newCollection.save(function (err, coll) {
+                        if (err) return console.error(err);
+                        collID = coll._id
+                        res.status(200).json(collID)
+                    })
+                } catch (err) {res.status(500).json({message: err.message})}
             });
     })
+    }   catch (err) {res.status(500).json({message: err.message})}
 })
 
-async function createColl(req, res, u){
-    try {
-        var newCollection = new Collections({ formatType: req.body.formatType, visualizations: req.body.visualizations, createdBy: username});
-        newCollection.save(function (err, coll) {
-            if (err) return console.error(err);
-            res.status(200).json(coll._id)
-        })
-    } catch (err) {res.status(500).json({message: err.message})}
-}
+router.get('/test', async (req, res) => {
+    res.status(200).json.message("Yes")
+})
 
 //Get collection with id
-router.get('/:collectionID', async (req, res) => {
+router.get('/c/:collectionID', async (req, res) => {
     try {
         const result = await Collections.find({"_id": req.params.collectionID})
         res.status(200).json(result)
--- routes/user/userDelete_Route.js
+++ routes/user/userDelete_Route.js
@@ -2,32 +2,30 @@ const express = require('express')
 const config = require("../../auth.config");
 const router = express.Router()
 const User = require('../../models/user/user_Model')
-var bcrypt = require("bcryptjs");
+const Collections = require('../../models/collections/collections_Model')
 var jwt = require("jsonwebtoken");
 
-
-router.get('/', async (req, res) => {
-    var u = null
+router.post('/', async (req, res) => {
     User.findOne({
         username: req.body.username
       })
         .exec((err, user) => {
-            u = user
             let token = req.headers["x-access-token"];
-            if (!token) {
-            return res.status(403).send({ message: "No token provided!" });
-            }
-        
+            if (!token) return res.status(403).send({ message: "No token provided!" });
+
             jwt.verify(token, config.secret, (err, decoded) => {
-            if (err) {
-                return res.status(401).send({ message: "Unauthorized!" });
-            }
-            req.userId = decoded.id;
-            deleteUser(req, res)
+                if (err) return res.status(401).send({ message: "Unauthorized!" });
+                req.userId = decoded.id;
+                deleteUserCollections(req, res, user)
             });
     })
 })
-
+async function deleteUserCollections(req, res, user){
+    try {
+        const result = await Collections.deleteMany({"createdBy": req.body.username})
+        deleteUser(req, res)
+    } catch (err) {res.status(500).json({message: err.message})}
+}
 async function deleteUser(req, res){
     try {
         const result = await User.deleteOne({"_id": req.userId });
